6edcbdf545f757d815ac661c97e7816f32d696b0
pascalacco <pascalacco@gmail.com>
2009-07-28 17:46:46 +0000

    ACCO + test_usartx_pol + test_usartx_irq + enleve /bin/goil* car bug sous windows...

---
+++ proj_cm3/test_USARTx_Pol/test_usart.c
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------
+ * Name:    test_usart.c
+ * Purpose: USART usage for STM32 with stdio.h
+ * Version: V1.00
+ *----------------------------------------------------------------------------
+ * ACCO [28 Juillet 2009] création et test en simulé sur USART 1,2 et 3 
+ *     c'est une dérive du projet boards/keil/stm32/USART_pol
+ *  utilise seulement des includes de la microlib.
+ * 
+ *----------------------------------------------------------------------------*/
+
+
+#include <stdio.h>
+
+#include "../../lib_cm3/lib_usartx_pol.h"
+
+/*----------------------------------------------------------------------------
+  MAIN function
+ *----------------------------------------------------------------------------*/
+int main (void) {
+
+                                 
+  usartx_setup();
+
+  printf ("Polling mode Serial I/O Example\r\n\r\n");
+
+  while (1) {                                     // Loop forever
+    unsigned char c;
+
+    printf ("Press a key. ");
+    c = getchar ();
+    printf ("\r\n");
+    printf ("You pressed '%c'.\r\n\r\n", c);
+  } // end while
+} // end main
