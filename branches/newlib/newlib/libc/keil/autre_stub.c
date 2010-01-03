52fa173719b0ab463129011d2b33cea20f1546ad
dimercur@insa-toulouse.fr <dimercur@insa-toulouse.fr@2346bbf8-76e5-11de-8103-5164bd6f55f0>
2010-01-03 21:13:51 +0000

    Ajout d'un projet keil pour compiler la newlib + les fichiers stub necessaires => compile mais non testé (et puis, ca pese 7 Ko)!

---
+++ branches/newlib/newlib/libc/keil/autre_stub.c
@@ -0,0 +1,87 @@
+void _free_r (void)
+{
+}
+
+void _svfprintf_r (void)
+{
+}
+
+void _svfiprintf_r (void)
+{
+}
+
+void _svfwprintf_r (void)
+{
+}
+
+void _vfprintf_r (void)
+{
+}
+
+void _vfwprintf_r (void)
+{
+}
+
+void __svfscanf_r (void)
+{
+}
+
+void __ssvfiscanf_r (void)
+{
+}
+
+void __svfwscanf_r (void)
+{
+}
+
+void __ssvfwscanf_r (void)
+{
+}
+
+void __ssvfscanf_r (void)
+{
+}
+
+void _malloc_r (void)
+{
+}
+
+void _realloc_r (void)
+{
+}
+
+void _calloc_r (void)
+{
+}
+
+void _valloc_r (void)
+{
+}
+
+void _pvalloc_r (void)
+{
+}
+
+void _memalign_r (void)
+{
+}
+
+void _malloc_usable_size_r (void)
+{
+}
+
+void _malloc_stats_r (void)
+{
+}
+
+void _mallopt_r (void)
+{
+}
+
+void _mallinfo_r (void)
+{
+}
+
+void _malloc_trim_r (void)
+{
+}
