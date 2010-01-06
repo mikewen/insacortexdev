d8e3e7c558fc08633e53c704f316e699d2d97a35
dimercur@insa-toulouse.fr <dimercur@insa-toulouse.fr@2346bbf8-76e5-11de-8103-5164bd6f55f0>
2010-01-06 12:10:33 +0000

    - suppression du repertoire newlib - ajout du repertoire minilib - travail en cours (stdio: 100%, ctypes/stdlib: 70%, string/sys: 0%)

---
+++ branches/newlib/minilib/include/missing_defs.h
@@ -0,0 +1,16 @@
+/*
+ * Todo: faire un commentaire d'introduction
+ */
+
+#ifndef __MISSING_DEFS__
+#define __MISSING_DEFS__
+
+/* Definition of FILE struct */
+struct __FILE
+{
+	int _file; // only field required for our lib C */
+};
+
+
+
+#endif /* __MISSING_DEFS__ */
