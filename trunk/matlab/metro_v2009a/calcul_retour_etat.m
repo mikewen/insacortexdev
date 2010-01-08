32e0e7f7fe00b8a153503051738ed7090a9fcf3e
pascalacco <pascalacco@2346bbf8-76e5-11de-8103-5164bd6f55f0>
2010-01-08 07:56:11 +0000

    babioles matlab

--- trunk/matlab/metro_v2009a/calcul_retour_etat.m
+++ trunk/matlab/metro_v2009a/calcul_retour_etat.m
@@ -1,25 +1,15 @@
 function [K,S,E,Ky,Sy,Ey]=calcul_retour_etat(K,Tau,N)
 
-%% Modèle d'état pour un moteur attaqué en courant
-%%      _
-%%  X = | Angle
-%%      | Vitesse Angulaire
-%%      |_
-%%
-%dim=2;
-%Ai = [0 1 ; 0 0];
-%Bi = [0; Kp/Ig] ;
-%Ci = [0 0   0 Ktach;
-%      1 0  -1 0];
+
 %% Pour ajouter un intégrateur, le vecteur d'état est donc
-%%      _
-%%      | Intégrale de l'angle
-%%  X = | Angle
-%%      | Vitesse Angulaire
-%%      |_
-%%
-%%  Y = | Vitesse
-%%      | Ecart devant
+%      _
+%      | Intégrale de l'angle
+%  X = | Angle
+%      | Vitesse Angulaire
+%      |_
+%
+%  Y = | Vitesse
+%      | Ecart devant
 dim=3;
 Ai = [0 1 0 ; 0 0 1; 0 0 -1/Tau];
 Bi = [0; 0;  K/Tau] ;
@@ -28,7 +18,7 @@ Ci = [0 0 0 0  0 1;
 
 A = zeros(dim*N);
 B = zeros(dim,1);
-C = zeros(2*N,dim*N);
+C = zeros(2*N+1,dim*N);
 for i=1:N 
     A(dim*i-dim+1:dim*i,dim*i-dim+1:dim*i)=Ai;
     B(dim*i-dim+1:dim*i,i)=Bi;
@@ -38,24 +28,28 @@ end
 % 1 à N-1 => Sortie en écarts
 C(1:(N-1),(dim-1):dim:dim*(N-1))=eye(N-1);
 C(1:(N-1),(2*dim-1):dim:dim*(N)) = C(1:(N-1),(2*dim-1):dim:dim*(N)) - eye(N-1) ;
-Q=eye(2*N)*10;
+Q=eye(2*N+1)*10;
 
 % N+1 à 2*N-1 => Sortie intégrale des écarts
 C(N+1:(2*N-1),1:N*dim-1)=C(1:(N-1),2:N*dim);
 Q(N+1:(2*N-1),N+1:(2*N-1))=Q(N+1:(2*N-1),N+1:(2*N-1))*100;
 
 % N => Position de la première trottinette
-C(N,:)=[ 0 1 0 zeros(1,dim*(N-1))];
+C(N,:)=[ 0 1 0 0 1 0 0 1 0];
 Q(N,N)=10;
 
 % 2*N => Intégrale de la position
-C(2*N,:)=[ 1 0 0 zeros(1,dim*(N-1))];
-Q(2*N,2*N)=100;
+C(2*N,:)=[1 0 0  1 0 0 1 0 0];
+Q(2*N,2*N)=Q(2*N,2*N)*100;
+
+% 2*N +1 => vitesse
+C(2*N+1,:)=[0 0 1  0 0 1 0 0 1];
+Q(2*N+1,2*N+1)=Q(2*N+1,2*N+1)*10;
 
 
 
-R=eye(N)*0.1;
-ntrot=ss(A,B,C,zeros(2*N,N));
+R=eye(N)*1;
+ntrot=ss(A,B,C,zeros(2*N+1,N));
 [Ky,Sy,Ey]=lqry(ntrot,Q,R);
 
 Q=eye(dim*N)*100;
--- trunk/matlab/metro_v2009a/lance_crash.m
+++ trunk/matlab/metro_v2009a/lance_crash.m
@@ -1,41 +1,68 @@
 clear all ;
 close all;
+clc;
 
-Te=2E-3; % temps d'échantillonnage
+
+Te=2E-3; % période d'échantillonnage de la commande
 
 
-Delta=10;
-Dist=32000;
-tda=0.5;
-tpsm=2;
-Vmax=1000;
-tdd=Dist/Vmax;
-Amax = Vmax/tpsm;
-L=tdd+tpsm+2;
+Delta=10; %% écart initial entre les loco au démarrage
+
+%% Initialisation du générateur de trajectoire
+%   Génère des consignes de position et de vitesse cohérentes selon le profil 
+%   de vitesse suivant
+% 
+% Vitesse
+%   /\         Vmax ----> _______________________________
+%    |                  / I                             I\
+%    |                 /  I                             I \
+%    |                /	  I                             I  \
+%    |			     /	  I                             I   \
+%    |		  	    /	  I 							I	 \
+%    |   __________/	  I                             I     \____________
+%    |_____________I______I_____________________________I___________________\ temps
+%                  |       |_                           |<---->|_          /
+%                  |		 \_                         |		  \_
+%                  |           \_                       |		    \_
+%                  |<----------->|                      |<----------> |
+%                  |  tpsm   					   	    |      tpsm 
+%                 tda                                   tdd
+%
+% L'intégrale de ce profil de vitesse donne une position qui doit se
+% terminer à la valeur Dist désirée
+%   Surface = Position finale = Dist = (tdd-tda)*Vmax
+
+Dist=32000; % distance à parcourir
+tda=0.5;    %temps début d'accélération (s)
+tpsm=2;     %temps de montée (s)
+Vmax=1000;  % Vitesse Maximale (pas/s)
+tdd=tda+Dist/Vmax; % temps début de descente
+Amax = Vmax/tpsm;  % accélération maximale (pas/s/s)
+L=tdd+tpsm+2;   % horizon de simulation
 
 Vitmax = 1200 %% pas/s
 
 
-%% Valeurs des N modèles
-N=3;
-Knom = 0.4979; %% Gain trottinette
-sigma_K = Knom/100;
-K=(randn(N,1)*sigma_K)+Knom;
-K(end)=K(end)*1;
+%% Valeurs des N modèles de locomotive
+% Vit(p)          K
+% _______  =  ____________
+%  Com(p)      1 + Tau . p
+
+N=3; % nombre de loco simulées
+
+%% initialise N gains K autour de la valeur nominale
+Knom = 0.4979;          % Gain nominal
+sigma_K = Knom/100;     % 1% devariance de K sur l'univers probabiliste des différentes locos
+K=(randn(N,1)*sigma_K)+Knom; % Vecteur des N gains K de chaque loco simulée
+K(end)=K(end)*1;   
 
 
-Taunom=3E-2; %% 
-%% Vit(p) = K /(1+Tm p )
-
-sigma_Tau=Taunom/100;
-Tau=(randn(N,1)*sigma_Tau)+Taunom;
+%% initialise N constante Tu autour de la valeur nominale
+Taunom=3E-2; % constante de temps d'une loco 
+sigma_Tau=Taunom/10; % forte variance de 10% car il peut y avoir
+                     % beaucoup de monde dans une voiture de métro
+Tau=(randn(N,1)*sigma_Tau)+Taunom; % Vecteur des N constante de temps Tau des locos
 Tau(end) = 1*Tau(end);
-
-if (N>1) 
-    M_ecart=[eye(N-1) zeros(N-1,1)]-[zeros(N-1,1) eye(N-1)];
-else
-    M_ecart=0;
-end;
 
 
 [Kr,S,E,Kry,Sy,Ey]=calcul_retour_etat(Knom,Taunom,N);
@@ -47,13 +74,10 @@ Kv=Kr(:,dim:dim:dim*N)
 
 
 sim n_metro_discret;
-Puissance_LQ = sum(commandes_courant.*commandes_courant)/length(commandes_courant)
+Puissance_LQ = sum(commandes.*commandes)/length(commandes)
 Puissance_Tot_LQ = sum(Puissance_LQ)/N
 visu_crash;
 
-
-plot(temps,ecarts);
-hold on;
 pause;
 close all;
 hold off;
@@ -61,8 +85,7 @@ Ki=Kry(:,1:dim:dim*N)*(dim-2)
 Ka=Kry(:,dim-1:dim:dim*N)
 Kv=Kry(:,dim:dim:dim*N)
 sim n_metro_discret;
-Puissance_LQY = sum(commandes_courant.*commandes_courant)/length(commandes_courant)
+Puissance_LQY = sum(commandes.*commandes)/length(commandes)
 Puissance_Tot_LQY = sum(Puissance_LQY)/N
 
-%plot(temps,ecarts,'--')
  visu_crash
