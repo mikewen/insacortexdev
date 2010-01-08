24450a64e7eb3a708b8814f9f279f8b2f3d942ff
pascalacco <pascalacco@2346bbf8-76e5-11de-8103-5164bd6f55f0>
2010-01-08 07:56:11 +0000

    babioles matlab

--- matlab/metro_v2009a/visu_crash.m
+++ matlab/metro_v2009a/visu_crash.m
@@ -1,19 +1,19 @@
 close all;
-plot(temps,ecarts);
-hold on;
-plot(temps,angles_consignes(:,1)-angles(:,1));
+plot(temps,positions_consignes(:,1)-positions(:,1));
 title('ecarts');
 figure
+plot(temps,vitesses_consignes,'r')
+hold on
 plot(temps,vitesses);
 title('vitesses');
 figure;
-plot(temps,angles_consignes,'r')
+plot(temps,positions_consignes,'r')
 title('positions');
 hold on
-plot(temps,angles)
+plot(temps,positions)
 
 figure;
-plot(temps,commandes_courant)
+plot(temps,commandes)
 hold on
 plot([temps(1) temps(length(temps))],[4096 4096])
 title('commande');
