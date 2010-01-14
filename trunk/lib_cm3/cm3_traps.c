f38689abe90133baf33bdd94414a8bc42c6538a1
dimercur@insa-toulouse.fr <dimercur@insa-toulouse.fr@2346bbf8-76e5-11de-8103-5164bd6f55f0>
2010-01-14 12:41:42 +0000

    - Ajout dans Handler_Fault d'une routine de dump des registres systemes (core dump)

--- trunk/lib_cm3/cm3_traps.c
+++ trunk/lib_cm3/cm3_traps.c
@@ -29,8 +29,27 @@ ________________________________________________________________________________
 
 #include <stm32f10x_lib.h>
 #include "cm3_traps.h"
+#include <stdio.h>
+
+#define CORE_DUMP
+#ifdef CORE_DUMP
+
+#define SCB_BASE_ADDR	0xE000ED00
+#define CFSR_REG_ADDR 	0xE000ED28
+#define HFSR_REG_ADDR	0xE000ED2C
+#define MMFAR_REG_ADDR	0xE000ED34
+#define BFAR_REG_ADDR	0xE000ED38
+
+u32 CFSR_REG;
+u32 HFSR_REG;
+u32 MMFAR_REG;
+u32 BFAR_REG;
+u32 *ptr;
+
+u32 R0_REG,R1_REG,R2_REG,R3_REG,R12_REG,SP_REG,LR_REG,RETURN_ADDR_REG,XPSR_REG;
 
 
+#endif /* CORE_DUMP */
 /*--------------------------Blink leds function --------------------*/
 
 #ifdef USE_BLINK_LEDS
@@ -61,10 +80,70 @@ void Blink_Leds(int repet, int duree)
 
 /*---------------------- Hardware Fault Handle ------------------*/
 
- #ifdef HANDLE_HARDWARE_FAULT
+#ifdef HANDLE_HARDWARE_FAULT
 
- void HardFault_Handler(void)
- {
+void HardFault_Handler(void)
+{
+ 	#ifdef CORE_DUMP
+
+		asm ("movw	r5, #:lower16:R0_REG;\n\t"
+			 "movt	r5, #:upper16:R0_REG;\n\t"
+			 "str 	r0 , [r5, #0];\n\t" /* Sauvegarde R0 dans R0_REG */
+			 "movw	r5, #:lower16:R1_REG;\n\t"
+			 "movt	r5, #:upper16:R1_REG;\n\t"
+			 "str 	r1 , [r5, #0];\n\t"/* Sauvegarde R1 dans R1_REG */
+			 "movw	r5, #:lower16:R2_REG;\n\t"
+			 "movt	r5, #:upper16:R2_REG;\n\t"
+			 "str 	r2 , [r5, #0];\n\t" /* Sauvegarde R2 dans R2_REG */
+			 "movw	r5, #:lower16:R3_REG;\n\t"
+			 "movt	r5, #:upper16:R3_REG;\n\t"
+			 "str 	r3 , [r5, #0];\n\t"/* Sauvegarde R3 dans R3_REG */
+			 "movw	r5, #:lower16:R12_REG;\n\t"
+			 "movt	r5, #:upper16:R12_REG;\n\t"
+			 "str 	r12 , [r5, #0];\n\t" /* Sauvegarde R12 dans R12_REG */);
+
+		asm ("ldr	r0,[sp,#17*4];\n\t" /* Charge dans R0 la valeur de LR poussé sur la stack */
+			 "movw	r5, #:lower16:LR_REG;\n\t"
+			 "movt	r5, #:upper16:LR_REG;\n\t"
+			 "str	r0,[r5,#0];\n\t"
+			 "ldr	r0,[sp,#18*4];\n\t" /* Charge dans R0 la valeur de l'adresse fautive poussé sur la stack */
+			 "movw	r5, #:lower16:RETURN_ADDR_REG;\n\t"
+			 "movt	r5, #:upper16:RETURN_ADDR_REG;\n\t"
+			 "str	r0,[r5,#0];\n\t"
+			 "ldr	r0,[sp,#19*4];\n\t" /* Charge dans R0 la valeur de PSR poussé sur la stack */
+			 "movw	r5, #:lower16:XPSR_REG;\n\t"
+			 "movt	r5, #:upper16:XPSR_REG;\n\t"
+			 "str	r0,[r5,#0];\n\t"
+		    );
+
+		ptr = (u32*)CFSR_REG_ADDR;
+		CFSR_REG = *ptr;
+		ptr = (u32*)HFSR_REG_ADDR;
+		HFSR_REG = *ptr;
+		ptr = (u32*)MMFAR_REG_ADDR;
+		MMFAR_REG = *ptr;
+		ptr = (u32*)BFAR_REG_ADDR;
+		BFAR_REG = *ptr;
+
+		printf ("\n\nHardFault Core Dump\n\n");
+		printf ("R0=%08X\nR1=%08X\nR2=%08X\nR3=%08X\nR12=%08X\nLR=%08X\nFaultAddr=%08X\nPSR=%08X\n\n",
+		         (unsigned int)R0_REG, 
+				 (unsigned int)R1_REG,
+				 (unsigned int)R2_REG, 
+				 (unsigned int)R3_REG,
+				 (unsigned int)R12_REG, 
+				 (unsigned int)LR_REG,
+				 (unsigned int)RETURN_ADDR_REG, 
+				 (unsigned int)XPSR_REG);
+
+		printf ("CFSR=%08X\nHFSR=%08X\nMMFAR=%08X\nBFAR=%08X\n",
+		         (unsigned int)CFSR_REG,
+				 (unsigned int)HFSR_REG,
+				 (unsigned int)MMFAR_REG,
+				 (unsigned int)BFAR_REG);
+
+	#endif /* CORE_DUMP */
+
  	#ifdef USER_HARDWARE_FAULT_HANDLER
 		 HARDWARE_FAULT_FUNCTION ;		
 	#else
