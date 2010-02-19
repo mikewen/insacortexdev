32dd473a600a5d7b0f140eb19a19fae6966c4fa3
pascalacco@gmail.com <pascalacco@gmail.com@2346bbf8-76e5-11de-8103-5164bd6f55f0>
2010-02-19 13:57:24 +0000

    BAguette avec DMA et analog watchdog + appli pas encore fonctionnelle

--- trunk/lib_user/lib_baguette.c
+++ trunk/lib_user/lib_baguette.c
@@ -211,6 +211,16 @@ void Position_Curseur(int x, int y)
 int Current_ADC;
 int ADC_Array[3];
 
+#define AU_MIN 0
+#define MONTE  1
+#define AU_MAX   2
+#define DESCEND  3
+#define S_TB  ((short int)(1.0/3.3*4096.0))
+#define S_B  ((short int)(1.1/3.3*4096.0))
+#define S_H  ((short int)(2.6/3.3*4096.0))
+#define S_TH  ((short int)(2.9/3.3*4096.0))
+char phase ;
+
 int ADC_min, ADC_max;
 
 /* 
@@ -219,6 +229,98 @@ int ADC_min, ADC_max;
  * Entrée: 		Rien
  * Sortie: 		Rien
  */
+
+//___________________________________________
+//ADC1->CR1 bit description
+#define EOCIE 	(1<<5)
+#define AWDIE 	(1<<6)
+#define JEOCIE 	(1<<7)
+#define SCAN 	(1<<8)
+#define AWDSGL 	(1<<9)
+#define JAUTO 	(1<<10)
+#define DISCEN 	(1<<11)
+#define JDISCEN (1<<12)
+#define DISCNUM_SHIFT (13)
+#define DUALMOD_SHIFT (16)
+#define JAWDEN  (1<<22)
+#define AWDEN  (1<<23)
+
+//ADC1->CR2 bit description
+#define ADON 	(1<<0)
+#define CONT 	(1<<1)
+#define CAL 	(1<<2)
+#define RSTCAL 	(1<<3)
+#define DMA 	(1<<8)
+#define ALIGN 	(1<<11)
+#define JEXTSEL_SHIFT 	(12)
+#define JEXTTRIG (1<<15)
+#define EXTSEL_SHIFT (17)
+#define EXTSEL_ON_SWSTART (7<<EXTSEL_SHIFT)
+#define EXTTRIG  (1<<20)
+#define JSWSTART (1<<21)
+#define SWSTART  (1<<22)
+#define TSVREFE  (1<<23)
+
+//ADC1-SQR1 bit description
+#define SQX_MASK (0x001F)
+#define SQ13_SHIFT (0)
+#define SQ14_SHIFT (5)
+#define SQ15_SHIFT (10)
+#define SQ16_SHIFT (15)
+#define L_MASK (0x000F)
+#define L_SHIFT (20)
+
+//ADC1-SQR2 bit description
+#define SQ7_SHIFT (0)
+#define SQ8_SHIFT (5)
+#define SQ9_SHIFT (10)
+#define SQ10_SHIFT (15)
+#define SQ11_SHIFT (20)
+#define SQ12_SHIFT (25)
+
+//ADC1-SQR3 bit description
+#define SQ1_SHIFT (0)
+#define SQ2_SHIFT (5)
+#define SQ3_SHIFT (10)
+#define SQ4_SHIFT (15)
+#define SQ5_SHIFT (20)
+#define SQ6_SHIFT (25)
+
+//ADC1->SR bit description
+#define AWD    (1<<0)
+#define EOC    (1<<1)
+#define JEOC   (1<<2)
+#define JSTRT  (1<<3)
+#define STRT   (1<<4)
+
+//DMAx_Channelx->CCR bit description
+#define EN	(1<<0)
+#define TCIE	(1<<1)
+#define HTIE	(1<<2)
+#define TEIE	(1<<3)
+#define DIR	    (1<<4)
+#define CIRC	(1<<5)
+#define PINC	(1<<6)
+#define MINC	(1<<7)
+#define PSIZE_SHIFT	(8)
+#define PSIZE_IS_8BITS (0 << PSIZE_SHIFT)
+#define PSIZE_IS_16BITS (1 << PSIZE_SHIFT)
+#define PSIZE_IS_32BITS (2 << PSIZE_SHIFT)
+#define MSIZE_SHIFT	(10)
+#define MSIZE_IS_8BITS (0 << MSIZE_SHIFT)
+#define MSIZE_IS_16BITS (1 << MSIZE_SHIFT)
+#define MSIZE_IS_32BITS (2 << MSIZE_SHIFT)
+#define PL_SHIFT	(12)
+#define PL_IS_LOW   (0<<PL_SHIFT)
+#define PL_IS_MEDIUM   (1<<PL_SHIFT)
+#define PL_IS_HIGH   (2<<PL_SHIFT)
+#define PL_IS_VERYHIGH   (3<<PL_SHIFT)
+#define MEM2MEM	(1<<14)
+
+
+void Watch_For_Lower_Than(short int gap);
+void Watch_For_Higher_Than(short int gap);
+
 void Init_ADC (void) 
 {
   	Current_ADC = 0;
@@ -227,16 +329,83 @@ void Init_ADC (void)
 	ADC_max = 0;
 
   	RCC->APB2ENR |= RCC_APB2Periph_ADC1;            // enable peripheral clock for ADC1
+	// switch on adc module
+	ADC1->CR2 |= ADON ;
 
-  	ADC1->SQR1  = 0x00000000;                       // only one conversion
-  	ADC1->SMPR2 = 0x00000028;                       // set sample time channel1 (55,5 cycles)
-  	ADC1->SQR3  = 0x00000001;                       // set channel1 as 1st conversion
+ 	// continuous scan of channels 1,14,15
+  	ADC1->CR1  |= (SCAN);                      
+  	//ADC1->CR1  |= EOCIE;                       // use independant mode, EOCIE mode
+  	ADC1->CR2  |= (EXTSEL_ON_SWSTART | CONT | DMA);  // use data align right,continuous conversion
+                                               // EXTSEL = SWSTART 
+                                               // enable ADC, no external Trigger
+											   // send DMA request
+ 
+  	ADC1->SQR1 |= ((3-1) & L_MASK)<<L_SHIFT;        // sequence of 3 conversion
+  	//	ADC1->SMPR2 = 0x00000028;                       // set sample time channel1 (55,5 cycles)
+  	
+	// channel 1 then 14 then 15
+	ADC1->SQR3  |= ((1 & SQX_MASK)<<SQ1_SHIFT) | ((14 & SQX_MASK)<<SQ2_SHIFT) | ((15 & SQX_MASK)<<SQ3_SHIFT);                       // set channel1 as 1st conversion
 
-  	ADC1->CR1   = 0x00000020;                       // use independant mode, EOCIE mode
-  	ADC1->CR2   = 0x000E0001;                       // use data align right,continuous conversion
-                                                  	// EXTSEL = SWSTART 
-                                                  	// enable ADC, no external Trigger
-  	ADC1->CR2  |=  0x00500000;					  	// start SW conversion
+  
+
+	// setup ADC guard on channel 14
+	ADC1->CR1 |= 14;
+	
+	
+	// select single regular channel for guard
+	ADC1->CR1 |= (AWDSGL | AWDEN);
+	ADC1->CR1 &= ~(JAWDEN);
+	
+	// valid guard interrupt flag
+	//ADC1->CR1 |= (AWDIE);
+
+
+	//DMA configuration
+	RCC->AHBENR |=0x00000001;// DMA1 CLOCK ENABLE
+	DMA1_Channel1->CPAR =(u32)&(ADC1->DR); // @ source 
+ 	DMA1_Channel1->CCR |= (PSIZE_IS_16BITS | MSIZE_IS_16BITS);   //ADC1-CR is 16 bits 
+  	DMA1_Channel1->CMAR =(u32) ADC_Array;   // @destination
+	DMA1_Channel1->CCR &= ~(DIR); //from periph to memory		
+  	DMA1_Channel1->CCR |=CIRC;   //Circular mode 
+ 	DMA1_Channel1->CNDTR=3;
+  	DMA1_Channel1->CCR |=MINC;   //Circular mode 
+  	DMA1_Channel1->CCR |=PL_IS_MEDIUM;	// priority level
+ 	
+  	DMA1_Channel1->CCR |= EN;	// l'DMA bazar est en marche
+ 
+ 	ADC1->CR2  |=  (ADON);				   // start SW conversion
+
+	phase = MONTE;
+	Watch_For_Higher_Than(S_TH);
+		
+
+}
+
+void Watch_For_Higher_Than(short int gap)
+{
+	// invalid guard interrupt flag
+	ADC1->CR1 &= ~(AWDIE);
+
+	//High and low guard value
+	ADC1->HTR = (int)(gap) & 0xFFF;
+	ADC1->LTR = 0;
+
+	// valid guard interrupt flag
+	ADC1->CR1 |= (AWDIE);
+
+}
+void Watch_For_Lower_Than(short int gap)
+{
+	// invalid guard interrupt flag
+	ADC1->CR1 &= ~(AWDIE);
+
+	//High and low guard value
+	ADC1->HTR = 0xFFF;
+	ADC1->LTR = (int)(gap) & 0xFFF;
+
+	// valid guard interrupt flag
+	ADC1->CR1 |= (AWDIE);
+
 }
 
 /* 
@@ -265,18 +434,37 @@ volatile register int tmp;
  * Entrée: 		Rien
  * Sortie: 		Rien
  */
+
+
 void ADC_IRQHandler	(void)
 {
-	ADC_Array[Current_ADC] = ADC1->DR;
-
-	Current_ADC++;
-	if (Current_ADC>=3) Current_ADC =0;
 	
-	if (Current_ADC == 0) ADC1->SQR3  = 1;                      // set Channel as 1st conversion
-	else if (Current_ADC == 1) ADC1->SQR3  = 14;                // set Channel as 1st conversion
-	else ADC1->SQR3  = 15; 
+	if (ADC1->SR & AWD)
+	{
+		ADC1->SR &= ~AWD;
+		switch (phase)
+		{
+			case MONTE : 
+				phase = AU_MAX;
+				Watch_For_Lower_Than(S_H);
+				break;
+			case AU_MAX : 
+				phase = DESCEND;
+				Watch_For_Lower_Than(S_TB);
+				break;
+			case DESCEND : 
+				phase = AU_MIN;
+				Watch_For_Higher_Than(S_B);
+				break;
+			case AU_MIN : 
+				phase = MONTE;
+				Watch_For_Higher_Than(S_TH);
+				break;
+			default :
+				break; 
+		}
+	}
 
-	ADC1->CR2  |=  0x00500000;					  				// start SW conversion
 }
 
 /* 
