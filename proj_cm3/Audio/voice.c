958d48660905358aecdacd4a6e100a529def4452
dimercur <dimercur@insa-toulouse.fr>
2010-08-01 20:15:51 +0000

    - Ajout d'un projet Video et un projet Audio: travaux en cours

---
+++ proj_cm3/Audio/voice.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) INSA Toulouse
+ * Author: Sebastien DI MERCURIO
+ *
+ * This file is part of MinOS.
+ *
+ * MinOS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * MinOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with MinOS; see the file COPYING.  If not,
+ * write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "stm_regs.h"
+#include "voice.h"
+
+
+struct VOICE_ST voices[_NB_VOICES_];
+
