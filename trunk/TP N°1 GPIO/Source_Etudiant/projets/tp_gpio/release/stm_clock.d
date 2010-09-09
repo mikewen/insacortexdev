160397f02a7d0bb7e6748a7ad3d6117af9a0967c
vincent.mahout@laposte.net <vincent.mahout@laposte.net@2346bbf8-76e5-11de-8103-5164bd6f55f0>
2010-09-09 12:01:12 +0000

    J'ajoute la version tu TP GPIO que je suppose "stable"

---
+++ trunk/TP N°1 GPIO/Source_Etudiant/projets/tp_gpio/release/stm_clock.d
@@ -0,0 +1,7 @@
+release/stm_clock.o: ../../pilotes/clock/stm_clock.c ../../sys/stm_regs.h \
+  ../../sys/standard_types.h \
+  C:/Keil/ARM/INC/ST/STM32F10x/stm32f10x_conf.h \
+  C:/Keil/ARM/INC/ST/STM32F10x/stm32f10x_type.h \
+  C:/Keil/ARM/INC/ST/STM32F10x/cortexm3_macro.h \
+  C:/Keil/ARM/INC/ST/STM32F10x/stm32f10x_type.h ../../sys/stm_system.h \
+  ../../pilotes/clock/stm_clock_config.h
---
+++ trunk/TP N°1 GPIO/Source_Etudiant/projets/tp_gpio/release/test_gpio.d
@@ -0,0 +1,6 @@
+release/test_gpio.o: src/Test_GPIO.c ../../sys/stm_regs.h \
+  ../../sys/standard_types.h \
+  C:/Keil/ARM/INC/ST/STM32F10x/stm32f10x_conf.h \
+  C:/Keil/ARM/INC/ST/STM32F10x/stm32f10x_type.h \
+  C:/Keil/ARM/INC/ST/STM32F10x/cortexm3_macro.h \
+  C:/Keil/ARM/INC/ST/STM32F10x/stm32f10x_type.h ../../sys/stm_system.h
