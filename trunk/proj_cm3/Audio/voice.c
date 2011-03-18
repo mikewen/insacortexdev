d5b6632913a50f8dc8688e0e2d3c890ebee51d4c
pascalacco <pascalacco@2346bbf8-76e5-11de-8103-5164bd6f55f0>
2011-03-18 12:33:32 +0000

    enmeve td2 de sujet3 + audio et filtre bug

---
+++ trunk/proj_cm3/Audio/voice.c
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) INSA Toulouse
+ * Author: Sebastien DI MERCURIO
+ *
+ * This file is part of MinOS.
+ *
+ * MinOS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * MinOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with MinOS; see the file COPYING.  If not,
+ * write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "stm_regs.h"
+#include "voice.h"
+#include "waveform.h"
+
+struct VOICE_ST voices[_NB_VOICES_];
+
+u16 note_array[5][12]=
+{
+	/* Do, Do#,  Ré,   Ré#,  Mi,   Fa,   Fa#,  Sol,  Sol#, La,   La#,  Si */
+	{8600, 8117, 7662, 7232, 6826, 6443, 6081, 5740, 5418, 5114, 4827, 4556},	/* octave 2 */
+	{4300, 4059, 3831, 3616, 3413, 3221, 3041, 2870, 2709, 2557, 2413, 2278},	/* octave 3 */
+	{2150, 2029, 1915, 1808, 1706, 1611, 1520, 1435, 1354, 1278, 1207, 1139},	/* octave 4 */
+	{1075, 1015, 958,  903,  853,  805,  760,  717,  677,  639,  603,  569 },	/* octave 5 */
+	{538,  507,  479,  452,  427,  403,  380,  359,  339,  320,  302,  284 }	/* octave 6 */
+};
+
+volatile u16 voice_buffer[4];
+u8 voice_note[4];
+u8 voice_octave[4];
+u16 voice_reload[4];
+u16 voice_counter[4];
+
+volatile int TIM3_cnt;
+
+volatile int SR;
+volatile int CCR1;
+volatile int CCMR1;
+volatile int CCMR2;
+volatile int CCER;
+volatile int CNT;
+
+int waveform_nbr;
+
+#define _PERIODE_PWM_TIM3_ 	72000000U
+#define PWM_MAX 			65535
+
+void Init_Voice (void)
+{
+	/* timer 3 : oscillateurs */
+	/* timer 2 : ADSR */
+	/* timer 1 : modulation fm */
+
+	/* Reglage du timer 1 -> Canal 0*/
+	RCC->APB2ENR |= RCC_TIM1EN; /* Mise en route de l'horloge du timer 1 */
+
+	TIM1->CNT = 0; /* On cale le timer juste apres (pas de risque de se prendre une IT avant la fin de l'init) */
+	TIM1->PSC = 0;
+	TIM1->ARR = PWM_MAX; 
+
+	TIM1->SMCR |= TIM_SMS_IS_DISABLED;	/* Desactivation du SMS */	
+
+	TIM1->DIER |= TIM_UIE; /* Active les IT overflow */
+
+	/* Reglage du timer 2 -> Canal 1*/
+	RCC->APB1ENR |= RCC_TIM2EN; /* Mise en route de l'horloge du timer 3 */
+
+	TIM2->CNT = 0; /* On cale le timer juste apres (pas de risque de se prendre une IT avant la fin de l'init) */
+	TIM2->PSC = 0;
+	TIM2->ARR = PWM_MAX; 
+
+	TIM2->SMCR |= TIM_SMS_IS_DISABLED;	/* Desactivation du SMS */	
+
+	TIM2->DIER |= TIM_UIE; /* Active les IT overflow */
+
+	/* Reglage du timer 3 -> Canal 2*/
+	RCC->APB1ENR |= RCC_TIM3EN; /* Mise en route de l'horloge du timer 3 */
+
+	TIM3->CNT = 0; /* On cale le timer juste apres (pas de risque de se prendre une IT avant la fin de l'init) */
+	TIM3->PSC = 0;
+	TIM3->ARR = PWM_MAX; 
+
+	TIM3->SMCR |= TIM_SMS_IS_DISABLED;	/* Desactivation du SMS */	
+
+	TIM3->DIER |= TIM_UIE; /* Active les IT overflow */ 
+
+	waveform_nbr = 2;
+}
+
+void Regle_Canal(int canal, int note, int octave)
+{
+	if (note==_VOICE_OFF_)
+	{
+		voice_buffer[canal]=0;
+		voice_reload[canal]=0;
+
+		switch (canal)
+		{
+		case 0:
+			TIM1->CR1 &= ~TIM_CEN;
+			break;
+		case 1:
+			TIM2->CR1 &= ~TIM_CEN;
+			break;
+		case 2:
+			TIM3->CR1 &= ~TIM_CEN;
+			break;
+		}
+	}
+	else
+	{
+		voice_reload[canal]=note_array[octave][note];
+
+		switch (canal)
+		{
+		case 0:
+			TIM1->ARR = voice_reload[canal];
+			TIM1->CR1 |= TIM_CEN;
+			break;
+		case 1:
+			TIM2->ARR = voice_reload[canal];
+			TIM2->CR1 |= TIM_CEN;
+			break;
+		case 2:
+			TIM3->ARR = voice_reload[canal];
+			TIM3->CR1 |= TIM_CEN;
+			break;
+		}
+	}	
+}
+
+void TIM1_UP_IRQHandler (void)
+{
+	if (TIM1->SR & TIM_UIF)
+	{
+		TIM1->SR = TIM1->SR & ~(TIM_UIF);		
+
+		voice_buffer[0]=Waveforms[waveform_nbr][voice_counter[0]];
+		voice_counter[0]++;
+		if (voice_counter[0]>=64) voice_counter[0]=0;
+	}
+}
+
+void TIM2_IRQHandler (void)
+{
+	if (TIM2->SR & TIM_UIF)
+	{
+		TIM2->SR = TIM2->SR & ~(TIM_UIF);		
+
+		voice_buffer[1]=Waveforms[waveform_nbr][voice_counter[1]];
+		voice_counter[1]++;
+		if (voice_counter[1]>=64) voice_counter[1]=0;
+	}
+}
+
+void TIM3_IRQHandler (void)
+{
+	if (TIM3->SR & TIM_UIF)
+	{
+		TIM3->SR = TIM3->SR & ~(TIM_UIF);		
+
+		voice_buffer[2]=Waveforms[waveform_nbr][voice_counter[2]];
+		voice_counter[2]++;
+		if (voice_counter[2]>=64) voice_counter[2]=0;
+	}
+}
---
+++ trunk/proj_cm3/Audio/waveform_gen/waveform_gen/waveform_gen/Program.cs
@@ -0,0 +1,225 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: Seb
+ * Date: 01/08/2010
+ * Heure: 13:52
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+using System;
+using System.IO;
+
+namespace waveform_gen
+{
+	class Program
+	{
+		static float[] Table;
+		static int index;
+		static int nb_waveform;
+		
+		// create a writer and open the file
+        static TextWriter C_file;
+		static TextWriter H_file;
+		
+		static int Taille;
+		static float Taille_f;
+		
+		public static void Main(string[] args)
+		{
+			Taille = 64;
+			Taille_f = (float)64.0;
+			
+			Table = new float[Taille];
+			C_file = new StreamWriter("waveform.c");
+			H_file = new StreamWriter("waveform.h");
+			
+			nb_waveform=0;
+			
+			C_file.WriteLine("/*");
+			C_file.WriteLine(" * Copyright (C) INSA Toulouse");
+			C_file.WriteLine(" * Author: Sebastien DI MERCURIO");
+			C_file.WriteLine(" *");
+			C_file.WriteLine(" * This file is part of MinOS.");
+			C_file.WriteLine(" *");
+			C_file.WriteLine(" * MinOS is free software; you can redistribute it and/or");
+			C_file.WriteLine(" * modify it under the terms of the GNU General Public");
+			C_file.WriteLine(" * License as published by the Free Software Foundation;");
+			C_file.WriteLine(" * either version 2, or (at your option) any later version.");
+			C_file.WriteLine(" *");
+			C_file.WriteLine(" * MinOS is distributed in the hope that it will be useful,");
+			C_file.WriteLine(" * but WITHOUT ANY WARRANTY; without even the implied");
+			C_file.WriteLine(" * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR");
+			C_file.WriteLine(" * PURPOSE.  See the GNU General Public License for more");
+			C_file.WriteLine(" * details.");
+			C_file.WriteLine(" *");
+			C_file.WriteLine(" * You should have received a copy of the GNU General Public");
+			C_file.WriteLine(" * License along with MinOS; see the file COPYING.  If not,");
+			C_file.WriteLine(" * write to the Free Software Foundation, Inc., 51 Franklin Street,");
+			C_file.WriteLine(" * Fifth Floor, Boston, MA 02110-1301, USA.");
+			C_file.WriteLine(" */\n");
+			
+			C_file.WriteLine("#include \"stm_regs.h\"\n#include \"waveform.h\"\n");
+			C_file.WriteLine("const u32 Waveforms[_WAVEFORMS_NB_][_WAVEFORMS_LENGTH_] =\n{");
+			
+			C_file.Write("\t{ // Triangle");
+				nb_waveform++;
+				
+				for (index=0;index<Taille/2;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write((int)(((float)index)*4096.0/(Taille_f/2))+",");
+				}
+				
+				for (index=Taille/2;index>0;index--)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write((int)(((float)index)*4096.0/(Taille_f/2))+",");
+				}
+			C_file.WriteLine("\n\t},");
+
+			C_file.Write("\t{ // Sinus");
+				nb_waveform++;
+				
+				for (index=0;index<Taille/2;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+
+                    C_file.Write((int)((Math.Sin((float)index * 2* Math.PI / Taille_f)) * 2048.0 + 2048.0) + ",");
+				}
+				
+				for (index=Taille/2;index<Taille;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+
+                    C_file.Write((int)((Math.Sin((float)index * 2* Math.PI / Taille_f)) * 2048.0 + 2048.0) + ",");
+				}
+			C_file.WriteLine("\n\t},");
+			
+			C_file.Write("\t{ // Square");
+				nb_waveform++;
+				
+				for (index=0;index<Taille/2;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write(((int)4096.0)+",");
+				}
+				
+				for (index=Taille/2;index<Taille;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write(((int)0.0)+",");
+				}
+			C_file.WriteLine("\n\t},");
+			
+			C_file.Write("\t{ // Sawteeth");
+				nb_waveform++;
+				
+				for (index=0;index<Taille/2;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write((int)(((float)index)*4096.0/(Taille_f/2))+",");
+				}
+				
+				for (index=0;index<Taille/2;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write((int)(((float)index)*4096.0/(Taille_f/2))+",");
+				}
+			C_file.WriteLine("\n\t},");
+			
+			C_file.Write("\t{ // Half sinus");
+				nb_waveform++;
+				
+				for (index=0;index<Taille/2;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write((int)((Math.Sin((float)index*Math.PI/(Taille_f/2)))*2048.0)+",");
+				}
+				
+				for (index=Taille/2;index<Taille;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write(((int)0.0)+",");
+				}
+			C_file.WriteLine("\n\t},");
+			
+			C_file.Write("\t{ // Double positive sinus");
+				nb_waveform++;
+				
+				for (index=0;index<Taille/2;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write((int)((Math.Sin((float)index*Math.PI/(Taille_f/2)))*4096.0)+",");
+				}
+				
+				for (index=0;index<Taille/2;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write((int)((Math.Sin((float)index*Math.PI/(Taille_f/2)))*4096.0)+",");
+				}
+			C_file.WriteLine("\n\t},");
+			
+			C_file.Write("\t{ // Half sawteeth");
+				nb_waveform++;
+				
+				for (index=0;index<Taille/2;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write((int)(((float)index)*4096.0/(Taille_f/2))+",");
+				}
+				
+				for (index=Taille/2;index<Taille;index++)
+				{
+					if (index%8==0) C_file.Write("\n\t");
+					
+					C_file.Write(((int)0.0)+",");
+				}
+			C_file.WriteLine("\n\t},");
+			
+			C_file.WriteLine("};\n");
+			// close the stream
+            C_file.Close();
+            
+            H_file.WriteLine("/*");
+			H_file.WriteLine(" * Copyright (C) INSA Toulouse");
+			H_file.WriteLine(" * Author: Sebastien DI MERCURIO");
+			H_file.WriteLine(" *");
+			H_file.WriteLine(" * This file is part of MinOS.");
+			H_file.WriteLine(" *");
+			H_file.WriteLine(" * MinOS is free software; you can redistribute it and/or");
+			H_file.WriteLine(" * modify it under the terms of the GNU General Public");
+			H_file.WriteLine(" * License as published by the Free Software Foundation;");
+			H_file.WriteLine(" * either version 2, or (at your option) any later version.");
+			H_file.WriteLine(" *");
+			H_file.WriteLine(" * MinOS is distributed in the hope that it will be useful,");
+			H_file.WriteLine(" * but WITHOUT ANY WARRANTY; without even the implied");
+			H_file.WriteLine(" * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR");
+			H_file.WriteLine(" * PURPOSE.  See the GNU General Public License for more");
+			H_file.WriteLine(" * details.");
+			H_file.WriteLine(" *");
+			H_file.WriteLine(" * You should have received a copy of the GNU General Public");
+			H_file.WriteLine(" * License along with MinOS; see the file COPYING.  If not,");
+			H_file.WriteLine(" * write to the Free Software Foundation, Inc., 51 Franklin Street,");
+			H_file.WriteLine(" * Fifth Floor, Boston, MA 02110-1301, USA.");
+			H_file.WriteLine(" */\n");
+			
+			H_file.WriteLine("#define _WAVEFORMS_NB_ "+nb_waveform+"\n#define _WAVEFORMS_LENGTH_ 128\n");
+			H_file.WriteLine("extern const u32 Waveforms[_WAVEFORMS_NB_][_WAVEFORMS_LENGTH_];\n");
+			H_file.Close();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
